053b1a24e9a7636ded8ba77fe6cbed5037ec43c4
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-07-08 19:10:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
-//your JS code here. If required.
 document.getElementById("btn").addEventListener("click", function () {
-	const num = Number(document.getElementById("ip").value);
+    const num = Number(document.getElementById("ip").value);
     const output = document.getElementById("output");
 
-	new Promise((resolve) => {
-		setTimeout(() => {
-			output.textContext = `Result: ${num}`;
-			resolve(num);
-		}, 2000);
-	})
-	.then((value) => {
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				value = value * 2;
-				output.textContent = `Result: ${value}`;
-				resolve(value);
-			}, 1000);
-		})
-	})
-	.then((value) => {
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				value = value - 3;
-				output.textContent = `Result: ${value}`;
-				resolve(value);
-			}, 1000);
-		})
-	})
-	.then((value) => {
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				value = value / 2;
-				output.textContent = `Result: ${value}`;
-				resolve(value);
-			}, 1000);
-		})
-	})
-	.then((value) => {
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				value = value + 10;
-				output.textContent = `Final Result: ${value}`;
-				resolve(value);
-			}, 1000);
-		})
-	})
-}
+    new Promise((resolve) => {
+        setTimeout(() => {
+            output.textContent = `Result: ${num}`;
+            resolve(num);
+        }, 2000);
+    })
+    .then((value) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                value *= 2;
+                output.textContent = `Result: ${value}`;
+                resolve(value);
+            }, 1000);
+        });
+    })
+    .then((value) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                value -= 3;
+                output.textContent = `Result: ${value}`;
+                resolve(value);
+            }, 1000);
+        });
+    })
+    .then((value) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                value /= 2;
+                output.textContent = `Result: ${value}`;
+                resolve(value);
+            }, 1000);
+        });
+    })
+    .then((value) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                value += 10;
+                output.textContent = `Final Result: ${value}`;
+                resolve(value);
+            }, 1000);
+        });
+    });
+});
